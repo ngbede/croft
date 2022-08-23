08b8c7649ea1ebc46a9042fd3490d5720127d155
ngbede <ogasule001@gmail.com>
2022-08-23 20:41:14 +0100

    untrack build folder

--- backend/build/db/init.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.supabaseServer = exports.supabase = void 0;
-const supabase_js_1 = require("@supabase/supabase-js");
-require('dotenv/config');
-exports.supabase = (0, supabase_js_1.createClient)(process.env.url, process.env.anon_key);
-// use for auth related stuff
-exports.supabaseServer = (0, supabase_js_1.createClient)(process.env.url, process.env.service_role);
--- backend/build/routes/farm.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const farm_controller_1 = require("../controllers/farm-controller");
-const farmRouter = (0, express_1.Router)();
-farmRouter.post("/farm/register", farm_controller_1.registerFarm);
-farmRouter.patch("/farm/:id", farm_controller_1.updateFarm);
-farmRouter.delete("/farm/:id", farm_controller_1.deleteFarm);
-exports.default = farmRouter;
