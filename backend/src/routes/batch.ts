be764e48854693f8a6f9dec6a0fa536ad362a96f
ngbede <ogasule001@gmail.com>
2022-08-27 22:25:39 +0100

    refactor batch controller pt 2

--- backend/src/routes/batch.ts
+++ backend/src/routes/batch.ts
@@ -1,10 +1,10 @@
 import { Router } from "express"
 import authenticator from "../middleware/auth/authenticator"
-import { createBatch, deleteBatch, getBatch, updateBatch, getBatchList } from "../controllers/batch-controller"
+import { createBatch, deleteBatch, getBatch, updateBatch } from "../controllers/batch-controller"
 
 const batchRouter = Router()
 
-batchRouter.get("/batch", authenticator, getBatchList) //?sort=ASC&page=2
+batchRouter.get("/batch", authenticator, getBatch) //?sort=ASC&page=2
 batchRouter.get("/batch/:id", authenticator, getBatch) // get all batches for a given farm id
 batchRouter.post("/batch/create", authenticator, createBatch)
 batchRouter.patch("/batch/:id", authenticator, updateBatch)
