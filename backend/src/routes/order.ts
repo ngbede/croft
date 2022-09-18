37d88da22272af07598361f4038c84793ce8a3ee
ngbede <ogasule001@gmail.com>
2022-09-18 23:58:59 +0100

    add custom order module controller

--- backend/src/routes/order.ts
+++ backend/src/routes/order.ts
@@ -1,6 +1,10 @@
 import { Router } from 'express'
 import authenticator from '../middleware/auth/authenticator'
-import { getOrder, createOrder, updateOrder } from '../controllers/order-controller'
+import {
+  getOrder,
+  createOrder,
+  updateOrder,
+} from '../controllers/order-controller'
 
 const orderRouter = Router()
 
@@ -8,3 +12,5 @@ orderRouter.get('/order/:id', authenticator, getOrder) // get single order doc
 orderRouter.post('/order/create', authenticator, createOrder)
 orderRouter.patch('/order/:id', authenticator, updateOrder)
 // orderRouter.delete('order/:id', authenticator) // we shouldn't outrightl delete order entities
+
+export default orderRouter
