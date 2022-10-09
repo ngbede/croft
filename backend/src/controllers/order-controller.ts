2b648b56b659a82f036be8fbd4b0d876e5c1c947
ngbede <ogasule001@gmail.com>
2022-10-09 15:46:56 +0100

    lint refactor

--- backend/src/controllers/order-controller.ts
+++ backend/src/controllers/order-controller.ts
@@ -4,26 +4,14 @@ import OrderController from '../modules/order/controller'
 
 const orderControl: OrderController = new OrderController('orders', 'order')
 
-export const createOrder = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+export const createOrder = async (req: Request, res: Response, next: NextFunction) => {
   return await orderControl.post(req, res, next, orderSchema)
 }
 
-export const getOrder = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+export const getOrder = async (req: Request, res: Response, next: NextFunction) => {
   return await orderControl.get(req, res, next)
 }
 
-export const updateOrder = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+export const updateOrder = async (req: Request, res: Response, next: NextFunction) => {
   return await orderControl.patch(req, res, next)
 }
--- backend/src/controllers/user-controller.ts
+++ backend/src/controllers/user-controller.ts
@@ -4,35 +4,19 @@ import UserController from '../modules/user/controller'
 
 const userControl: UserController = new UserController('user')
 
-export const getUserViaId = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+export const getUserViaId = async (req: Request, res: Response, next: NextFunction) => {
   return await userControl.get(req, res, next)
 }
 
-export const createUser = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+export const createUser = async (req: Request, res: Response, next: NextFunction) => {
   return await userControl.create(req, res, next, userSchema)
 }
 
-export const resetPassword = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+export const resetPassword = async (req: Request, res: Response, next: NextFunction) => {
   return await userControl.passwordReset(req, res, next)
 }
 
-export const signIn = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+export const signIn = async (req: Request, res: Response, next: NextFunction) => {
   return await userControl.login(req, res, next)
 }
 
