b50176d3def014d01af09bb231902eea01de1172
ngbede <ogasule001@gmail.com>
2022-10-09 08:19:38 +0100

    fix broken pg initialization

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,11 +5,11 @@ import 'dotenv/config'
 
 app.listen(process.env.PORT, async () => {
   try {
-    // await pgInstance.connect().then((_) => {
+    await pgInstance.connect().then((_) => {
       if (supabaseServer && supabase)
         console.log(`Running on port ${process.env.PORT}`)
       else console.log('client failed to connect')
-    //})
+    })
   } catch (error) {
     throw new Error(`${error}`)
   }
