6846828529ca87f6e91711e7e2fbe60ffc467f55
ngbede <ogasule001@gmail.com>
2022-10-29 00:07:47 +0100

    add paystack rest interface

---
+++ backend/src/services/payment/paystack.ts
@@ -0,0 +1,69 @@
+import RestAdapter from '../rest-adapter'
+
+interface customer {
+  email?: string
+  first_name?: string
+  last_name?: string
+  phone?: string
+}
+
+export default class PaystackInterface {
+  private restAdapter: RestAdapter
+  private endpoints = {
+    transaction: '/transaction',
+    initTransaction: '/transaction/initialize',
+    verifyTransaction: '/transaction/verify',
+    customer: '/customer',
+    identification: '/identification',
+    validate: '/bank/validate'
+  }
+
+  constructor(restAdapter: RestAdapter) {
+    this.restAdapter = restAdapter
+  }
+
+  async initiatePayment(paymentData: object) {
+    return this.restAdapter.create(this.endpoints.initTransaction, paymentData)
+  }
+
+  async verifyPayment(reference: string) {
+    const url = `${this.endpoints.verifyTransaction}/${reference}`
+    return this.restAdapter.read(url)
+  }
+
+  async listAllTransactions() {
+    return this.restAdapter.read(this.endpoints.transaction)
+  }
+
+  async getTransaction(id: string) {
+    const url = `${this.endpoints.transaction}/${id}`
+    return this.restAdapter.read(url)
+  }
+
+  async createCustomer(customerData: customer) {
+    return this.restAdapter.create(this.endpoints.customer, customerData)
+  }
+
+  async listAllCustomers() {
+    return this.restAdapter.read(this.endpoints.customer)
+  }
+
+  async getCustomer(id: string) {
+    const url = `${this.endpoints.customer}/${id}`
+    return this.restAdapter.read(url)
+  }
+
+  async updateCustomer(id: string, customerData: customer) {
+    const url = `${this.endpoints.customer}/${id}`
+    return this.restAdapter.put(url, customerData)
+  }
+
+  async validateCustomer(id: string, customerData: object) {
+    const url = `${this.endpoints.customer}/${id}${this.endpoints.identification}`
+    return this.restAdapter.create(url, customerData)
+  }
+
+  async validateAccount(accountData: object) {
+    return this.restAdapter.create(this.endpoints.validate, accountData)
+  }
+}
