986cd7a13017dae5eb7d550a74b35ca3f66bc577
ngbede <ogasule001@gmail.com>
2022-10-28 23:59:19 +0100

    add error handling to rest adapter

--- backend/src/services/rest-adapter.ts
+++ backend/src/services/rest-adapter.ts
@@ -1,4 +1,4 @@
-import axios from 'axios'
+import axios, { AxiosError } from 'axios'
 
 export default class RestAdapter {
 	baseUrl: string
@@ -11,34 +11,72 @@ export default class RestAdapter {
 		this.header = header || { 'Authorization': this.token }
 	}
 
+	// throw cleaner response from integrating rest api
+	private _errorHandle(error: unknown) {
+		if (error instanceof AxiosError) {
+			const err = { ...error.response?.data, code: error.response?.status }
+			throw err
+		}
+	}
+
 	async create(resource: string, payload: object) {
-		const data = await axios.post(`${this.baseUrl}${resource}`, payload, {
-			headers: this.header
-		})
-		return data
+		try {
+			return await axios.post(`${this.baseUrl}${resource}`, payload, {
+				headers: this.header
+			})
+		} catch (error) {
+			this._errorHandle(error)
+			console.log('house of dragons')
+			throw error
+		}
 	}
 
 	async read(resource: string, params?: object) {
-		const data = await axios.get(`${this.baseUrl}${resource}`, {
-			params: params,
-			headers: this.header
-		})
-		return data
+		try {
+			return await axios.get(`${this.baseUrl}${resource}`, {
+				params: params,
+				headers: this.header
+			})
+		} catch (error) {
+			this._errorHandle(error)
+			throw error
+		}
 	}
 
 	async update(resource: string, params?: object) {
-		const data = await axios.patch(`${this.baseUrl}${resource}`, {
-			params: params,
-			headers: this.header
-		})
-		return data
+		try {
+			return await axios.patch(`${this.baseUrl}${resource}`, {
+				params: params,
+				headers: this.header
+			})
+		} catch (error) {
+			this._errorHandle(error)
+			throw error
+		}
+
 	}
 
 	async delete(resource: string, params?: object) {
-		const data = await axios.delete(`${this.baseUrl}${resource}`, {
-			params: params,
-			headers: this.header
-		})
-		return data
+		try {
+			return await axios.delete(`${this.baseUrl}${resource}`, {
+				params: params,
+				headers: this.header
+			})
+		} catch (error) {
+			this._errorHandle(error)
+			throw error
+		}
+	}
+
+	async put(resource: string, params?: object) {
+		try {
+			return await axios.put(`${this.baseUrl}${resource}`, {
+				params: params,
+				headers: this.header
+			})
+		} catch (error) {
+			this._errorHandle(error)
+			throw error
+		}
 	}
 }
